6c6f1debc1c542952f2651c22d2f9d61c26b6069
Alexandra Sabatin <s4batin@gmail.com>
2022-10-16 03:05:00 +0300

    add update contact status

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -1,12 +1,12 @@
-const contacts = require("../../models/contacts");
+const { Contact } = require("../../models/contacts");
 
 const { RequestError } = require("../../helpers");
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) {
-    throw RequestError(404);
+    throw RequestError(404, "Contact not found");
   }
   res.json(result);
 };
--- controllers/contacts/updateStatus.js
+++ controllers/contacts/updateStatus.js
@@ -2,15 +2,18 @@ const { Contact } = require("../../models/contacts");
 
 const { RequestError } = require("../../helpers");
 
-const updateById = async (req, res) => {
+const updateFavorite = async (req, res) => {
   const { contactId } = req.params;
+  if (!req.body) {
+    throw RequestError(400, "Missing field favorite");
+  }
   const result = await Contact.findByIdAndUpdate(contactId, req.body, {
     new: true,
   });
   if (!result) {
-    throw RequestError(404, "Contact not found");
+    throw RequestError(404);
   }
   res.json(result);
 };
 
-module.exports = updateById;
+module.exports = updateFavorite;
